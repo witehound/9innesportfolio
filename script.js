b9be370d2794fd163c5d50260a5af158890961f2
witehound <netvid00@gmail.com>
2023-08-01 14:33:09 +0100

    done with rles modals

---
+++ script.js
@@ -0,0 +1,15 @@
+const openRules = document.getElementById("rule-footer");
+const ruleModal = document.getElementById("rules");
+const closeIcon = document.getElementById("close-icon");
+
+openRules.addEventListener("click", () => {
+  ruleModal.style.display = "block";
+  ruleModal.style.display = "flex";
+  openRules.style.display = "none";
+});
+
+closeIcon.addEventListener("click", () => {
+  ruleModal.style.display = "none";
+
+  openRules.style.display = "block";
+});

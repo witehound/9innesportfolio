bb62b5b186fdd6a4bc677d62318bc644be564088
witehound <netvid00@gmail.com>
2023-08-03 18:06:25 +0100

    latest update

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const openRules = document.getElementById("rule-footer");
 const ruleModal = document.getElementById("rules");
 const closeIcon = document.getElementById("close-icon");
-
 const game = document.getElementById("game");
-
 const hand = document.getElementById("hand");
 const scissors = document.getElementById("scissors");
 const rock = document.getElementById("rock");
-
 const gameScreen = document.getElementById("game-screen");
-
 const userScreenItem = document.getElementById("user-screen-item");
 const computerScreenItem = document.getElementById("computer-screen-item");
+
+const scroreBoard = document.getElementById("scrore-board");
+
+let totalScore = 0;
+
+const updateFromLocalStorage = () => {
+  let temp =
+    localStorage.getItem("result") == null
+      ? 0
+      : (totalScore = localStorage.getItem("result"));
+
+  scroreBoard.innerHTML = temp;
+};
+
+const setLocatStorage = (number) => {
+  localStorage.setItem("result", number);
+};
 
 const data = [
   [hand, "hsl(230, 89%, 62%), hsl(230, 89%, 65%)", "./images/icon-paper.svg"],
@@ -22,6 +35,20 @@ const data = [
   ],
   [rock, "hsl(349, 71%, 52%), hsl(349, 70%, 56%)", "images/icon-rock.svg"],
 ];
+
+const calResult = (user, computer) => {
+  if (user == computer) return 2;
+  if (user == 0) {
+    if (computer == 1) return 0;
+    return 1;
+  } else if (user == 1) {
+    if (computer == 2) return 0;
+    return 1;
+  } else {
+    if (computer == 0) return 0;
+    return 1;
+  }
+};
 
 const cgn = Math.floor(Math.random() * 2);
 
@@ -61,17 +88,28 @@ const move = (bgColor, src, userScreenItem) => {
   userScreenItem.appendChild(user);
 };
 
-function play(type, bgColor, src, userScreenItem) {
+function play(type, bgColor, src, userScreenItem, user) {
   type.addEventListener("click", () => {
     move(bgColor, src, userScreenItem);
 
-    setTimeout(
-      () => move(data[cgn][1], data[cgn][2], computerScreenItem),
-      2000
-    );
+    const res = calResult(user, cgn);
+
+    setTimeout(() => {
+      move(data[cgn][1], data[cgn][2], computerScreenItem);
+      if (res == 1) {
+        totalScore++;
+        console.log(totalScore);
+        setLocatStorage(totalScore);
+        scroreBoard.innerHTML = totalScore;
+      } else if (res == 2) {
+      } else {
+      }
+    }, 2000);
   });
 }
 
+updateFromLocalStorage();
+
 for (let i = 0; i < data.length; i++) {
-  play(data[i][0], data[i][1], data[i][2], userScreenItem);
+  play(data[i][0], data[i][1], data[i][2], userScreenItem, i);
 }
